20a072b7b3f1f6efc4c3a149bbef03e27a7490c9
Jordane YENO <104835883+JordaneYeno@users.noreply.github.com>
2025-07-13 10:04:13 +0100

    'd'

--- app/Enums/ServiceStatus.php
+++ app/Enums/ServiceStatus.php
@@ -6,7 +6,7 @@ class ServiceStatus
 {
     const RESET = 0;     // Réinitialisé
     const PENDING = 1;   // En attente
-    const REJECTED = 2;  // Rejeté
+    const REJECTED = 2;  // Rejeté 
     const ACCEPTED = 3;  // Accepté
 
     // Optionnel : méthodes d’aide pour faciliter l’utilisation dans les contrôleurs
